f3867b9d6b779eee608fd45e9325b85fe31c9a54
Peter Geiss <peter.w.geiss@gmail.com>
2015-06-10 04:01:58 -0400

    .gitignore added

--- localhost.js
+++ localhost.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var app = express();
 
-app.set('port', 80);
+app.set('port', 8000);
 app.use(express.static(__dirname));
 
 app.get('/', function (req, res) {
-	res.sendFile(__dirname + req.url);
+	res.sendFile(__dirname + '/public/index.html');
+})
+
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/public/' + req.url);
+});
+
+app.listen(app.get('port'), function () {
+	console.log('Node app is running at port ' + app.get('port'));
 });
--- server.js
+++ server.js
@@ -5,5 +5,13 @@ app.set('port', 80);
 app.use(express.static(__dirname));
 
 app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get(/\/public\/*/, function (req, res) {
 	res.sendFile(__dirname + req.url);
+});
+
+app.listen(app.get('port'), function () {
+	console.log('Node app is running at port ' + app.get('port'));
 });
