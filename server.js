db22dda3df4928549b30022f69a58b968707d9e9
Peter Geiss <peter.w.geiss@gmail.com>
2015-06-10 05:05:18 -0400

    Routers seem to be working, tests updated

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 app.set('port', 80);
 app.use(express.static(__dirname));
 
 app.get('/', function (req, res) {
-	res.sendFile(__dirname + '/public/index.html');
+	'use strict';
+	res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.get(/\/public\/*/, function (req, res) {
-	res.sendFile(__dirname + req.url);
+app.get(/\w*\.\.\w*/, function (req, res) {
+	'use strict';
+	res.end("The pattern '..' is prohibited in URLs.");
+});
+
+app.get(/\/\w*/, function (req, res) {
+	'use strict';
+	res.sendFile(path.join(__dirname, "public/", req.url));
 });
 
 app.listen(app.get('port'), function () {
+	'use strict';
 	console.log('Node app is running at port ' + app.get('port'));
 });
