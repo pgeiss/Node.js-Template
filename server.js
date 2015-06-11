29eaf9728d9a9ceded80c2f66511929268d82b1e
Peter Geiss <peter.w.geiss@gmail.com>
2015-06-11 02:06:24 -0400

    Send error messages properly, update placeholders

--- server.js
+++ server.js
@@ -6,21 +6,35 @@ app.set('port', 80);
 app.use(express.static(__dirname));
 
 app.get('/', function (req, res) {
-	'use strict';
-	res.sendFile(path.join(__dirname, '/public/index.html'));
+	res.sendFile(path.join(__dirname, '/public/index.html'), function (err) {
+		if (err) {
+			console.log(err);
+			res.status(err.status).send("Oops. Something unexpected happened \
+				-- error " + err.status + ".");
+		}
+		else {
+			//Nothing
+		}
+	});
 });
 
 app.get(/\w*\.\.\w*/, function (req, res) {
-	'use strict';
 	res.end("The pattern '..' is prohibited in URLs.");
 });
 
 app.get(/\/\w*/, function (req, res) {
-	'use strict';
-	res.sendFile(path.join(__dirname, "public/", req.url));
+	res.sendFile(path.join(__dirname, "public/", req.url), function (err) {
+		if (err) {
+			console.log(err);
+			res.status(err.status).send("Oops. Something unexpected happened \
+				-- error " + err.status + ".");
+		}
+		else {
+			//Nothing
+		}
+	});
 });
 
 app.listen(app.get('port'), function () {
-	'use strict';
 	console.log('Node app is running at port ' + app.get('port'));
 });
